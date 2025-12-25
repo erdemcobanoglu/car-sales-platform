8d18b2e1b8607c439e871e92ce63d2a878e6df12
erdem çobanoglu <erdemcobanoglu1122@hotmail.com>
2025-12-25 14:55:40 +0000

    data seeder added and added program.cs conf

---
+++ CarSalesPlatform/Presentation/Data/Seed/DbSeeder.cs
@@ -0,0 +1,67 @@
+﻿using Presentation.Models;
+using Presentation.Models.Enums;
+
+namespace Presentation.Data.Seed;
+
+public static class DbSeeder
+{
+    public static async Task SeedAsync(AppDbContext db)
+    {
+        // Lookup data zaten varsa tekrar basma
+        if (db.Makes.Any())
+            return;
+
+        // ===== MAKE =====
+        var ford = new Make
+        {
+            Name = "Ford"
+        };
+
+        // ===== MODEL =====
+        var smax = new VehicleModel
+        {
+            Name = "S-Max",
+            Make = ford
+        };
+
+        // ===== TRIM =====
+        var zetec = new Trim
+        {
+            Name = "Zetec",
+            Level = "Base Trim",
+            Model = smax
+        };
+
+        db.Makes.Add(ford);
+        db.Models.Add(smax);
+        db.Trims.Add(zetec);
+
+        // ===== VEHICLE (TEST DATA) =====
+        var vehicle = new Vehicle
+        {
+            Make = ford,
+            Model = smax,
+            Trim = zetec,
+
+            Year = 2017,
+            Mileage = 91344,
+            MileageUnit = MileageUnit.Miles,
+
+            EngineLiters = 2.0,
+            FuelType = FuelType.Diesel,
+            Transmission = TransmissionType.Automatic,
+            BodyType = BodyType.MPV,
+
+            Seats = 7,
+            Doors = 5,
+            Colour = "Silver",
+
+            TotalOwners = 2,
+            NctExpiry = new DateOnly(2025, 11, 1)
+        };
+
+        db.Vehicles.Add(vehicle);
+
+        await db.SaveChangesAsync();
+    }
+}
--- CarSalesPlatform/Presentation/Program.cs
+++ CarSalesPlatform/Presentation/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Presentation.Data;
+using Presentation.Data.Seed;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("Default")
+    ));
 
 var app = builder.Build();
 
@@ -9,7 +18,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -23,5 +31,13 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+/* ===== MIGRATION + SEED ===== */
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+    await DbSeeder.SeedAsync(db);
+}
 
 app.Run();
